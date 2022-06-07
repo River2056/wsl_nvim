ef15ea1fc7ccc08df1a5c7ac771819a76ee17f5a
River2056 <chen0625tung@gmail.com>
2022-06-07 03:06:36 +0000

    + added config: auto nvm and vim-plug

--- createLink.sh
+++ createLink.sh
@@ -1,3 +1,8 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+nvm install node
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/nvim/init
 mkdir -p ~/.config/nvim/lua/kevin
